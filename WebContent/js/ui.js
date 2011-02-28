fdaac4686758e113659784f68d97736306acbff3
Miguel Andrade <jmandrade0@gmail.com>
2011-02-28 21:00:53 +0000

    Chat finalizado.
    Signed-off-by: Miguel Andrade <jmandrade0@gmail.com>

--- WebContent/js/ui.js
+++ WebContent/js/ui.js
@@ -7,14 +7,15 @@ function toggle(){
     }
     else {
         // actual click event code
-		var value,value2;
-		if(visible) {value=440;value2=0}
-		else {value=175;value2=264}
+		var value,value2,value3;
+		if(visible) {value=440;value2=0;value3=0.7}
+		else {value=175;value2=264;value3=1}
 		visible=!visible;
 		//$( "#slidebottom" ).draggable( "option", "disabled", !visible );
 	
 		$("#slidebottom").animate({
 			top:value,
+			opacity:value3,
 		}, 1500 );
 		/*$("#chat").animate({
 			height:value2,
